e01ca022274ee156195fb4ee113ecdb62eed5690
Koltai Gergely Álmos <almos@revolin.io>
2022-09-18 13:21:11 +0200

    Export components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button/Button";
+export { default as AppointmentCalendar } from "./AppointmentCalendar/AppointmentCalendar";
+export { default as AppointmentModal } from "./AppointmentModal/AppointmentModal";
+export { default as Icon } from "./Icon/Icon";
+export { default as Input } from "./Input/Input";
+export { default as Modal } from "./Modal/Modal";
