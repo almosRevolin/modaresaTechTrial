4fb78b1cdb43d247a42438bb5723122a53a26648
Koltai Gergely Álmos <almos@revolin.io>
2022-09-18 15:34:09 +0200

    Export select

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as AppointmentModal } from "./AppointmentModal/AppointmentModal
 export { default as Icon } from "./Icon/Icon";
 export { default as Input } from "./Input/Input";
 export { default as Modal } from "./Modal/Modal";
+export { default as Select } from "./Select/Select";
