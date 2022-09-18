1ce7a5cd600019db80145be154c0f586165f6dd7
Koltai Gergely Álmos <almos@revolin.io>
2022-09-18 13:20:16 +0200

    Impl create appointment component

---
+++ src/components/CreateAppointment/CreateAppointment.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { AppointmentModal, Button } from "../index";
+
+const CreateAppointment = ({
+  campaign,
+  createAppointment,
+}: CreateAppointmentProps) => {
+  const {
+    _id,
+    startDate,
+    endDate,
+    vendorId,
+    defaultAppointmentDuration,
+    title,
+  } = campaign;
+
+  const baseInputs = {
+    title: "",
+    start: new Date(),
+    end: new Date(),
+    duration: defaultAppointmentDuration,
+    campaignId: _id,
+  };
+
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
+  const [appointment, setAppointment] = useState<AppointmentInputs>(baseInputs);
+
+  const handleSaveNewAppointment = () => {
+    createAppointment(appointment);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setAppointment(prevAppointment => ({ ...prevAppointment, [name]: value }));
+  };
+
+  const handleCreateClick = () => {
+    setIsCreateModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsCreateModalOpen(false);
+    setAppointment(baseInputs);
+  };
+
+  console.log(isCreateModalOpen);
+
+  return (
+    <>
+      <div className="mt-4">
+        <Button onClick={handleCreateClick}>Create appointment</Button>
+      </div>
+      {isCreateModalOpen && (
+        <AppointmentModal
+          isOpen={isCreateModalOpen}
+          onClose={handleCloseModal}
+          appointment={appointment}
+          saveAppointment={handleSaveNewAppointment}
+          onInputChange={handleInputChange}
+          campaignTitle={title}
+        />
+      )}
+    </>
+  );
+};
+
+type CreateAppointmentProps = {
+  campaign: Campaign;
+  createAppointment: (appointment: AppointmentInputs) => void;
+};
+
+export default CreateAppointment;
